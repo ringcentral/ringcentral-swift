6b42456d6b8094075e24147f5248ab021e87cefa
Tyler Long <tyler4long@gmail.com>
2017-10-20 10:30:16 +0800

    Support MMS

--- Tests/FaxTest.swift
+++ Tests/FaxTest.swift
@@ -31,4 +31,30 @@ class FaxTest: BaseTest {
         }
     }
 
+    func testSendingMms() {
+        let expectation1 = expectation(description: "expectation1")
+        let expectation2 = expectation(description: "expectation2")
+
+        let parameters: Parameters = ["from": ["phoneNumber": Config.getInstance().username!], "to": [["phoneNumber": Config.getInstance().receiver!]]]
+        let postParameters = MmsPath.PostParameters(from: CallerInfo(phoneNumber: Config.getInstance().username!), to: [CallerInfo(phoneNumber: Config.getInstance().receiver!)])
+        var attachments: [Attachment] = []
+        Alamofire.download("https://assets-cdn.github.com/images/modules/about/about-header-2016.jpg").responseData { response in
+            if let textData = response.result.value {
+                attachments.append(Attachment(fileName: "test.jpg", contentType: "image/jpeg", data: textData))
+                rc.restapi().account().extension().mms().post(parameters: parameters, attachments: attachments) { messageInfo, error in
+                    XCTAssertNil(error)
+                    expectation1.fulfill()
+                }
+                rc.restapi().account().extension().mms().post(parameters: postParameters, attachments: attachments) { messageInfo, error in
+                    XCTAssertNil(error)
+                    expectation2.fulfill()
+                }
+            }
+        }
+
+        waitForExpectations(timeout: 20) { error in
+            XCTAssertNil(error)
+        }
+    }
+
 }
