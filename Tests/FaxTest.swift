875759c1dbe5b407095ac7ef38c8ebe863222fc3
Tyler Long <tyler4long@gmail.com>
2018-05-09 16:32:47 +0800

    Update dependencies so it works well with latest Swift.

--- Tests/FaxTest.swift
+++ Tests/FaxTest.swift
@@ -12,7 +12,7 @@ class FaxTest: BaseTest {
         let parameters: Parameters = ["to": [["phoneNumber": Config.getInstance().receiver!]]]
 //        let postParameters = FaxPath.PostParameters(to: [CallerInfo(phoneNumber: Config.getInstance().receiver!)])
         var attachments: [Attachment] = []
-        Alamofire.download("https://raw.githubusercontent.com/ringcentral/ringcentral-swift/master/README.md").responseData { response in
+        Alamofire.download("https://cdn.jsdelivr.net/npm/ringcentral@3.2.0/LICENSE.md").responseData { response in
             if let textData = response.result.value {
                 attachments.append(Attachment(fileName: "README.txt", contentType: "text/plain", data: textData))
                 rc.restapi().account().extension().fax().post(parameters: parameters, attachments: attachments) { messageInfo, error in
@@ -38,9 +38,9 @@ class FaxTest: BaseTest {
         let parameters: Parameters = ["from": ["phoneNumber": Config.getInstance().username!], "to": [["phoneNumber": Config.getInstance().receiver!]]]
         let postParameters = MmsPath.PostParameters(from: CallerInfo(phoneNumber: Config.getInstance().username!), to: [CallerInfo(phoneNumber: Config.getInstance().receiver!)])
         var attachments: [Attachment] = []
-        Alamofire.download("https://assets-cdn.github.com/images/modules/about/about-header-2016.jpg").responseData { response in
+        Alamofire.download("https://developer.ringcentral.com/assets/images/ico_case_business.png").responseData { response in
             if let textData = response.result.value {
-                attachments.append(Attachment(fileName: "test.jpg", contentType: "image/jpeg", data: textData))
+                attachments.append(Attachment(fileName: "test.png", contentType: "image/png", data: textData))
                 rc.restapi().account().extension().mms().post(parameters: parameters, attachments: attachments) { messageInfo, error in
                     XCTAssertNil(error)
                     expectation1.fulfill()
