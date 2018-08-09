35e5b50b5ff528a50b78a87645fd36b9c0ec6bd3
Tyler Long <tyler4long@gmail.com>
2018-08-09 16:42:33 +0800

    Simplify code, remove useless code

--- Source/Special.swift
+++ Source/Special.swift
@@ -77,29 +77,6 @@ open class MmsPath: PathSegment {
         let requestBody = parametersBody.data(using: String.Encoding.utf8)!
         post(requestBody: requestBody, attachments: attachments, callback: callback)
     }
-//    open class PostParameters: Mappable {
-//        // Sender of an SMS message. The phoneNumber property must be filled to correspond to one of the account phone numbers which is allowed to send SMS
-//        open var `from`: CallerInfo?
-//        // Receiver of an SMS message. The phoneNumber property must be filled
-//        open var `to`: [CallerInfo]?
-//        // Text of a message. Max length is 1000 symbols (2-byte UTF-16 encoded). If a character is encoded in 4 bytes in UTF-16 it is treated as 2 characters, thus restricting the maximum message length to 500 symbols
-//        open var `text`: String?
-//        public init() {
-//        }
-//        required public init?(map: Map) {
-//        }
-//        convenience public init(from: CallerInfo? = nil, to: [CallerInfo]? = nil, text: String? = nil) {
-//            self.init()
-//            self.from = `from`
-//            self.to = `to`
-//            self.text = `text`
-//        }
-//        open func mapping(map: Map) {
-//            `from` <- map["from"]
-//            `to` <- map["to"]
-//            `text` <- map["text"]
-//        }
-//    }
 }
 
 
@@ -150,11 +127,6 @@ extension FaxPath {
         let requestBody = try! JSONSerialization.data(withJSONObject: parameters)
         post(requestBody: requestBody, attachments: attachments, callback: callback)
     }
-//    open func post(parameters: PostParameters, attachments: [Attachment], callback: @escaping (_ t: MessageInfo?, _ error: HTTPError?) -> Void) {
-//        let parametersBody = parameters.toParameters()["json-string"] as! String
-//        let requestBody = parametersBody.data(using: String.Encoding.utf8)!
-//        post(requestBody: requestBody, attachments: attachments, callback: callback)
-//    }
 }
 
 
@@ -209,128 +181,4 @@ open class Notification: Mappable {
     open func mapping(map: Map) {
         event <- map["event"]
     }
-}
-
-//// Subscription notification models
-//public protocol INotification: Mappable {
-//}
-//public enum NotificationType: String {
-//    case DetailedPresence, DetailedPresenceWithSIP, ExtensionInfo, ExtensionList, IncomingCall, InstantMessage, Message, PresenceLine, Presence
-//}
-//extension DetailedPresenceNotification: INotification {
-//}
-//extension DetailedPresenceWithSIPNotification: INotification {
-//}
-//extension ExtensionInfoNotification: INotification {
-//}
-//extension ExtensionListNotification: INotification {
-//}
-//extension IncomingCallNotification: INotification {
-//}
-//extension InstantMessageNotification: INotification {
-//}
-//extension MessageNotification: INotification {
-//}
-//extension PresenceLineNotification: INotification {
-//}
-//extension PresenceNotification: INotification {
-//}
-//open class Notification: INotification {
-//    open var event: String?
-//    open var json: String?
-//
-//    required public init?(map: Map) {
-//
-//    }
-//
-//    convenience public init?(json: String) {
-//        self.init(JSONString: json)
-//        self.json = json
-//    }
-//
-//    open func mapping(map: Map) {
-//        event <- map["event"]
-//    }
-//
-//    private func presenceType() -> NotificationType? {
-//        if let event = self.event {
-//            let urlComponents = URLComponents(string: event)!
-//            let queryItems = urlComponents.queryItems
-//            var result: NotificationType = .Presence
-//            if let _ = queryItems?.filter({$0.name == "detailedTelephonyState"}).first {
-//                result = .DetailedPresence
-//            }
-//            if let _ = queryItems?.filter({$0.name == "sipData"}).first {
-//                result = .DetailedPresenceWithSIP
-//            }
-//            return result
-//        }
-//        return nil
-//    }
-//    open var type: NotificationType? {
-//        get {
-//            if let event = self.event {
-//                switch event {
-//                case Regex(pattern: "/account/\\d+/extension/\\d+/message-store/instant"):
-//                    return .InstantMessage
-//                case Regex(pattern: "/account/\\d+/extension/\\d+/message-store"):
-//                    return .Message
-//                case Regex(pattern: "/account/\\d+/extension/\\d+/presence/line/presence"):
-//                    return presenceType()
-//                case Regex(pattern: "/account/\\d+/extension/\\d+/presence/line"):
-//                    return .PresenceLine
-//                case Regex(pattern: "/account/\\d+/extension/\\d+/presence"):
-//                    return presenceType()
-//                case Regex(pattern: "/account/\\d+/extension/\\d+/incoming-call-pickup"):
-//                    return .IncomingCall
-//                case Regex(pattern: "/account/\\d+/extension/\\d+"):
-//                    return .ExtensionInfo
-//                case Regex(pattern: "account/\\d+/extension"):
-//                    return .ExtensionList
-//                default:
-//                    return nil
-//                }
-//            }
-//            return nil
-//        }
-//    }
-//
-//    // todo: any better way ? such as `as!` ?
-//    open func downcast<T: INotification>() -> T? {
-//        if let json = self.json {
-//            return T(JSONString: json)
-//        }
-//        return nil
-//    }
-//}
-
-
-
-// Utils
-struct Regex {
-    let pattern: String
-    let options: NSRegularExpression.Options!
-
-    private var matcher: NSRegularExpression {
-        return try! NSRegularExpression(pattern: self.pattern, options: self.options)
-    }
-    init(pattern: String, options: NSRegularExpression.Options = NSRegularExpression.Options.useUnixLineSeparators) {
-        self.pattern = pattern
-        self.options = options
-    }
-    func match(string: String, options: NSRegularExpression.MatchingOptions = NSRegularExpression.MatchingOptions.reportCompletion) -> Bool {
-        return self.matcher.numberOfMatches(in: string, options: options, range: NSMakeRange(0, string.utf16.count)) != 0
-    }
-}
-protocol RegularExpressionMatchable {
-    func match(regex: Regex) -> Bool
-}
-
-extension String: RegularExpressionMatchable {
-    func match(regex: Regex) -> Bool {
-        return regex.match(string: self)
-    }
-}
-func ~=<T: RegularExpressionMatchable>(pattern: Regex, matchable: T) -> Bool {
-    return matchable.match(regex: pattern)
 }
