5046d886bd9977a98d76a5d85ff3bcfe9dd44084
Tyler Long <tyler4long@gmail.com>
2020-11-14 20:46:20 -0800

    Refactor code

--- Sources/RingCentral/RingCentral.swift
+++ Sources/RingCentral/RingCentral.swift
@@ -24,35 +24,36 @@ public class RingCentral {
             "password": password,
             "grant_type": "password"
         ]
-        let base64Token = "\(self.options.clientId!):\(self.options.clientSecret!)".data(using: String.Encoding.utf8)!.base64EncodedString(options: [])
-        let headers: HTTPHeaders = [
-            "Authorization": "Basic \(base64Token)"
-        ]
-        AF.request("\(self.options.server!)/restapi/oauth/token", method: .post, parameters: parameters, encoding: URLEncoding.default, headers: headers).responseJSON { response in
+        AF.request("\(self.options.server!)/restapi/oauth/token", method: .post, parameters: parameters,
+                   encoding: URLEncoding.default, headers: getBasicHeaders()).responseJSON { response in
             self.tokenInfo = response.value as? [String: Any]
             callback?()
         }
     }
-    public func request(_ endpoint: String, method: HTTPMethod = .get,
-                      parameters: Parameters? = nil,
+    public func request(_ endpoint: String, method: HTTPMethod = .get, parameters: Parameters? = nil,
                       encoding: ParameterEncoding = JSONEncoding.default) -> DataRequest {
         let headers: HTTPHeaders = [
             "Authorization": "Bearer \(self.tokenInfo!["access_token"]!)"
         ]
-        return AF.request(self.options.server! + endpoint, method: method, parameters: parameters, encoding: encoding, headers: headers)
+        return AF.request(self.options.server! + endpoint, method: method,
+                          parameters: parameters, encoding: encoding, headers: headers)
     }
     public func refresh(callback: (() -> Void)? = nil) {
         let parameters: Parameters = [
             "grant_type": "refresh_token",
             "refresh_token": self.tokenInfo!["refresh_token"]!
         ]
+        AF.request("\(self.options.server!)/restapi/oauth/token", method: .post, parameters: parameters,
+                   encoding: URLEncoding.default, headers: getBasicHeaders()).responseJSON { response in
+            self.tokenInfo = response.value as? [String: Any]
+            callback?()
+        }
+    }
+    private func getBasicHeaders() -> HTTPHeaders {
         let base64Token = "\(self.options.clientId!):\(self.options.clientSecret!)".data(using: String.Encoding.utf8)!.base64EncodedString(options: [])
         let headers: HTTPHeaders = [
             "Authorization": "Basic \(base64Token)"
         ]
-        AF.request("\(self.options.server!)/restapi/oauth/token", method: .post, parameters: parameters, encoding: URLEncoding.default, headers: headers).responseJSON { response in
-            self.tokenInfo = response.value as? [String: Any]
-            callback?()
-        }
+        return headers
     }
 }
